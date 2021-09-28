e29c82a8f1330baca274c98baf1b7e1fd0b3d151
Yashrif Hasib <yashrif.hasib@stonybrook.edu>
2021-09-28 12:36:36 -0400

    Loading list items done

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 export default class Workspace extends React.Component {
+
     render() {
+
+        const { currentList } = this.props;
+        let items = ["", "", "", "", ""];
+        
+        if (currentList) {
+            items = [currentList.items[0], currentList.items[1], currentList.items[2], currentList.items[3], currentList.items[4]]
+        }
+ 
         return (
             <div id="top5-workspace">
                 <div id="workspace-edit">
@@ -11,6 +20,13 @@ export default class Workspace extends React.Component {
                         <div className="item-number">3.</div>
                         <div className="item-number">4.</div>
                         <div className="item-number">5.</div>
+                    </div>
+                    <div id="edit-items">
+                        <div className="item-1" className="top5-item"> { items[0] } </div>
+                        <div className="item-2" className="top5-item"> { items[1] } </div>
+                        <div className="item-3" className="top5-item"> { items[2] } </div>
+                        <div className="item-4" className="top5-item"> { items[3] } </div>
+                        <div className="item-5" className="top5-item"> { items[4] } </div>
                     </div>
                 </div>
             </div>
