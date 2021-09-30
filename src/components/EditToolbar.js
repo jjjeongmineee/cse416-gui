2ee13a4f06945bbb3b54f1a27453644abc3431ba
Yashrif Hasib <yashrif.hasib@stonybrook.edu>
2021-09-30 13:18:00 -0400

    button fucntion added

--- src/components/EditToolbar.js
+++ src/components/EditToolbar.js
@@ -1,22 +1,38 @@
 import React from "react";
 
 export default class EditToolbar extends React.Component {
+    handleUndo = () => {
+
+    }
+
+    handleRedo = () => {
+        
+    }
+
+    handleClose = () => {
+        
+    }
+
+    
     render() {
         return (
             <div id="edit-toolbar">
                 <div 
                     id='undo-button' 
-                    className="top5-button">
+                    className="top5-button"
+                    onClick={this.handleUndo}>
                         &#x21B6;
                 </div>
                 <div
                     id='redo-button'
-                    className="top5-button">
+                    className="top5-button"
+                    onClick={this.handleRedo}>
                         &#x21B7;
                 </div>
                 <div
                     id='close-button'
-                    className="top5-button">
+                    className="top5-button"
+                    onClick={this.handleClose}>
                         &#x24E7;
                 </div>
             </div>
