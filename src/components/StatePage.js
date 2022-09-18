174954f0c59481b5e679e09756ae5456fa098119
Theo Grossberndt <tgrossb87@gmail.com>
2022-09-18 17:18:49 -0400

    Added padding to states page and second tab view

--- src/components/StatePage.js
+++ src/components/StatePage.js
@@ -12,30 +12,35 @@ import MapTab from './MapTab.js';
 export default class StatePage extends React.Component {
 	constructor(props){
 		super(props);
-		this.state = {selectedTab: 0};
+		this.state = {mapSelectedTab: 0, dataSelectedTab: 0};
 	}
 
 	render(){
 		const stateName = this.props.stateName;
-		const selectedTab = this.state.selectedTab;
+		const mapSelectedTab = this.state.mapSelectedTab;
+		const dataSelectedTab = this.state.dataSelectedTab;
 
 		return (
 			<div className='stateRoot'>
-				<div className='mapHolder'>
+				<div className='halfHolder'>
 					<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-						<Tabs value={selectedTab} onChange={(e, v) => this.setState({selectedTab: v})} aria-label="basic tabs example">
+						<Tabs value={mapSelectedTab} onChange={(e, v) => this.setState({mapSelectedTab: v})} aria-label="map tabs">
 							<Tab label="Current Districting Plan" id='cdp' ariaControls='cdp'/>
 							<Tab label="Multi-Member Districting Plan" id='mmd' ariaControls='mmd'/>
 						</Tabs>
 					</Box>
 
-					<MapTab selectedTab={selectedTab} tabIndex={0} stateName={stateName}/>
-					<MapTab selectedTab={selectedTab} tabIndex={1} stateName={stateName}/>
+					<MapTab selectedTab={mapSelectedTab} tabIndex={0} stateName={stateName}/>
+					<MapTab selectedTab={mapSelectedTab} tabIndex={1} stateName={stateName}/>
 				</div>
-				<div className='dataHolder'>
-					<div className='dataChild'>
-						Data
-					</div>
+
+				<div className='halfHolder'>
+					<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+						<Tabs value={dataSelectedTab} onChange={(e, v) => this.setState({dataSelectedTab: v})} aria-label="data tabs">
+							<Tab label="Fairness Metrics" id='fm' ariaControls='fm'/>
+							<Tab label="Other stuff?" id='os' ariaControls='os'/>
+						</Tabs>
+					</Box>
 				</div>
 			</div>
 		);
