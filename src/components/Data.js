044c029a474dc66ffb975e135250dd66c88260a8
Yashrif Hasib <yashrifhasib@gmail.com>
2022-09-18 22:15:06 -0400

    added louisiana and mississippi data. District bounds will have to be changed because they are outdated. Precinct data and interactive mechanism has not been setup yet.

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,32 +2,43 @@
  * A temporary setup for prototyping.  Takes the place of the server.
  */
 
+// louisiana data
 import louisianaStateBounds from './geojson/louisianaStateBounds.json';
+import louisianaCountyBounds from './geojson/louisianaCountyBounds.json';
+import louisianaDistrictBounds from './geojson/louisianaDistrictBounds.json';
 
+// nevada data
 import nevadaStateBounds from './geojson/nevadaStateBounds.json';
 import nevadaCountyBounds from './geojson/nevadaCountyBounds.json';
 import nevadaDistrictBounds from './geojson/nevadaDistrictBounds.json'
 
+// mississippi data
 import mississippiStateBounds from './geojson/mississippiStateBounds.json';
+import mississippiCountyBounds from './geojson/mississippiCountyBounds.json';
+import mississippiDistrictBounds from './geojson/mississippiDistrictBounds.json';
 
 var Data;
 export default Data = {
 	'Louisiana': {
 		center: [30.58, -92.32],
 		zoom: 7,
-		stateBounds: louisianaStateBounds
+		stateBounds: louisianaStateBounds,
+		countyBounds: louisianaCountyBounds,
+		districtBounds: louisianaDistrictBounds
 	},
 	'Nevada': {
 		center: [38.629, -116.631],
 		zoom: 7,
 		stateBounds: nevadaStateBounds,
-		districtBounds: nevadaDistrictBounds,
-		countyBounds: nevadaCountyBounds
+		countyBounds: nevadaCountyBounds,
+		districtBounds: nevadaDistrictBounds
 	},
 	'Mississippi': {
 		center: [32.697, -89.655],
 		zoom: 7,
-		stateBounds: mississippiStateBounds
+		stateBounds: mississippiStateBounds,
+		countyBounds: mississippiCountyBounds,
+		districtBounds: mississippiDistrictBounds
 	}
 };
 
